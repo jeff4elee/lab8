2d9987409c677f033b98f69e33018cecf3067976
jeffrey lee <jeffreylee@kingyoorekismbp.dynamic.ucsd.edu>
2018-03-01 03:06:02 -0800

    lab8 done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,8 +1,7 @@
 // 'use strict';
-
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-  initializePage();
+$(document).ready(function () {
+	initializePage();
 })
 
 /*
@@ -10,17 +9,37 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	initCamera();
- 	initGestures();
- 	initRSVPForm();
+	initCamera();	
+	initGestures();
+	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function () {
+		$(".judge-img").bind("taphold", tapholdHandler);
+		function tapholdHandler(event) {
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
+
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	// add your code here
+	$('#rsvpForm').submit(function(e) {
+		e.preventDefault();
+		console.log("submitting form...");
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+
+	function postCallback(res) {
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
 }
